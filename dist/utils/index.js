e32b7266f87756799bc7ddf7c291597e7b842b7a
Yao <yao@cryptape.com>
2018-08-30 10:58:34 +0800

    update: remove log

--- dist/utils/index.js
+++ dist/utils/index.js
@@ -1,5 +1,6 @@
 var log = console.log.bind(console)
-// var log = () => {}
+// 使用下面的 log 替代, 从而取消 log
+var log = () => {}
 
 log.title = (title) => {
     return log.bind(console, `-- ${title} --\n`)
